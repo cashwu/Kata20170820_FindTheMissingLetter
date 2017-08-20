e40c05524e0e8cbe776920b4b527c5c174be48ab
cash <cash@cashwu.com>
2017-08-20 15:01:09 +0800

    input b, d test case

--- Kata20170820_FindTheMissingLetter/FindTtheMissingLetterTests.cs
+++ Kata20170820_FindTheMissingLetter/FindTtheMissingLetterTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170820_FindTheMissingLetter
@@ -10,6 +11,12 @@ namespace Kata20170820_FindTheMissingLetter
         public void input_a_c_should_return_b()
         {
             FindMissingLetterShouldBe('b', new[] {'a', 'c'});
+        }
+
+        [TestMethod]
+        public void input_b_d_should_return_b()
+        {
+            FindMissingLetterShouldBe('c', new[] { 'b', 'd' });
         }
 
         private static void FindMissingLetterShouldBe(char expected, char[] array)
@@ -24,7 +31,19 @@ namespace Kata20170820_FindTheMissingLetter
     {
         public char FindMissingLetter(char[] array)
         {
-            return 'b';
+            var firstAsc2 = Convert.ToInt32(array[0]);
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (firstAsc2 - Convert.ToInt32(array[i]) < 0)
+                {
+                    return Convert.ToChar(firstAsc2 + 1);
+                }
+
+                firstAsc2++;
+            }
+
+            return 'a';
         }
     }
 }
