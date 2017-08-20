ce1b2d7d7c9794d50f9e28b2ad346040190ec457
cash <cash@cashwu.com>
2017-08-20 14:39:14 +0800

    init

--- Kata20170820_FindTheMissingLetter/FindTtheMissingLetterTests.cs
+++ Kata20170820_FindTheMissingLetter/FindTtheMissingLetterTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170820_FindTheMissingLetter
 {
     [TestClass]
-    public class UnitTest1
+    public class FindTtheMissingLetterTests
     {
         [TestMethod]
         public void TestMethod1()
